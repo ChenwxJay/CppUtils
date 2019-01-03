4574d76b7527f4d63a07ce5f4f4b4555e6a4c79e
ChenwxJay <chenwx_jay@163.com>
2019-01-03 23:56:24 +0800

    Add a header file about unique_ptr and a cpp file about auto_ptr

---
+++ Smart_ptr/auto_ptr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+template <class T>
+class My_auto_ptr{
+public:
+	My_auto_ptr(){
+		
+	}
+}
---
+++ Smart_ptr/unique_ptr.h
@@ -0,0 +1,4 @@
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+class 
